e36d760721d51c8ba636510a3c171d524a0efe5b
KaelWise <krw0094@auburn.edu>
2024-10-28 20:52:58 -0500

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -3,19 +3,48 @@
 using namespace std;
 
 class Temperature {
-	private:
-		double degreesKelvin,
-		       degreesFahrenheit,
-		       degreesCelsius;
-	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+private:
+    double degreesKelvin,
+           degreesFahrenheit,
+           degreesCelsius;
+public:
+    //Temperature Conversions
+    void setTempKelvin(double Kelvin) {
+        degreesKelvin = Kelvin;
+        degreesCelsius = degreesKelvin - 273.15;
+        degreesFahrenheit = (degreesCelsius * 9.0 / 5.0) + 32;
+    }
+    double getTempKelvin() {
+        return degreesKelvin;
+    }
+    void setTempCelsius() {
+
+    }
+    double getTempCelsius() {
+        return degreesCelsius;
+    }
+    void setTempFahrenheit() {
+
+    }
+    double getTempFahrenheit() {
+        return degreesFahrenheit;
+    }
 };
 
 int main() {
+    //Variable Defined
+    double Kelvin;
+    //Class Object Defined
+    Temperature temp;
+    //Temperature Entered and Set
+    cout << "Please enter a temperature in degrees Kelvin:";
+    cin >> Kelvin;
+    temp.setTempKelvin(Kelvin);
+    //Temperature Conversions Displayed
+    cout << endl << temp.getTempKelvin() << " Degrees Kelvin\n"
+    << temp.getTempCelsius() << " Degrees Celsius\n"
+    << temp.getTempFahrenheit() << " Degrees Fahrenheit\n";
+
+    return 0;
 
 }
